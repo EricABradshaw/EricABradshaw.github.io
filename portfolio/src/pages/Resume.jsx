1ea0b6a8d7b6ea8113e7b41e13625d7ba7dcd269
EricABradshaw <bradea02@gmail.com>
2024-04-19 16:11:43 -0400

    Added resume and basic layout of navigation

---
+++ portfolio/src/pages/Resume.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resume = () => {
+    return (
+        <div className='' style={{height: '100vh'}}>
+            <embed className='m-auto' src="/EricBradshaw_resume.pdf" type="application/pdf" width="100%" height="100%"/>
+        </div>
+    )
+}
+
+export default Resume
